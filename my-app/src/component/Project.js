5ef31de7a30990db2effed76c57f1dafd372f4ea
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-12-30 20:13:57 -0800

    adding my projects

--- my-app/src/component/Project.js
+++ my-app/src/component/Project.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-import despoc from '../images/despoc.jpg';
-import vems from '../files/vems.pdf';
-import face from '../files/facialRecognitionSmartLock.pdf';
-import artmos from '../files/artmos.pdf';
+import despoc from '../images/fixtreetimage.png';
+import fixtreet from '../files/fixTreet.pdf';
 import '../css/project.css';
 
 export default class Project extends Component{
@@ -15,20 +13,24 @@ export default class Project extends Component{
                     {/* Published Project BEGIN */}
                     <div className="card mb-3" style={{borderColor:'#f3e909'}} >
                         <div className="row">
-                            <div className="col-md-4">
-                                <img src={despoc} className="card-img despocImage" alt="DeSPOC"/>
-                            </div>
+                            
                             <div className="col-md-8">
                                 <div className="card-body">
-                                    <h5 className="card-title" style={{lineHeight:'2.0', textAlign:'left'}}>Publication</h5>
-                                    <p className="card-text" style={{lineHeight:'2.0', textAlign:'left', fontSize:"17px"}}>
-                                    <i><b>Experimental Validation of an IoT based Device Selective Power Cut Mechanism Using Power Line Carrier Communication for Smart Management of Electricity</b></i><br/>
-                                    This work was dealt with Implementing a device selective protocol which can selectively cut power of particular devices depending upon their power rating, 
-                                    manufacturer company etc. by use of power line carrier communication. Hence revolutionizing the concept of area selective power cut to device selective power cut.</p><br/>
-                                    <a className="btn viewbtn rounded-pill shadow" href="https://link.springer.com/article/10.1007/s42835-020-00568-6#:~:text=With%20device%20selective%20power%2Dcut,with%20limited%20use%20of%20electricity" 
-                                        target="_blank" style={{textDecoration:'none', width:'50%'}}>
-                                        <i className="fa fa-eye">&ensp;View Research Paper</i>
-                                    </a>                                
+                                   
+                                <img src={despoc} className="card-img despocImage" alt="DeSPOC"/>
+                                    <p className="card-text" style={{lineHeight:'2.0', textAlign:'middle', fontSize:"17px"}}>
+                                    <i><b>This Project was a group project and was combination of Hardware and Software. Our group got second position for our project throughout Kuwait. </b></i><br/>
+                                    Our project solves this problem of potholes using a system known as “Fixtreet” that
+                                    aims to automatically detect and classify potholes using a camera operated by the
+                                    driver. The system sends the severity level, picture, location, date and time of the
+                                    pothole automatically to the database that is then accessed by the admin using a
+                                    website. The admin, in turn, can take immediate actions in order to repair the
+                                    potholes. Fixtreet provides a relatively simple yet effective and efficient method to
+                                    get rid of potholes across the streets of Kuwait.
+                                    To visit the website please click on <a href="https://fixtreet.web.app/#team "style={{color:'#21AA47'}}>FixTreet.</a> </p><br/>
+                                    <a className="btn viewbtn rounded-pill shadow" href={fixtreet} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'68px'}}>
+                                    <i className="fa fa-eye">&ensp;View Project</i>
+                                </a>                       
                                 </div>
                             </div>
                         </div>
@@ -40,10 +42,10 @@ export default class Project extends Component{
                         <div className="card" style={{borderColor:'#f3e909'}} data-aos="fade-up">
                             {/* <img src="..." className="card-img-top" alt="..."/> */}
                             <div className="card-body">
-                                <h5 className="card-title">Vehicle Monitoring System (VEMS)</h5>
-                                <p className="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>VeMS is to track and monitor the vehicle parameters using IoT. 
-                                The parameter being object detection, weather forecast, engine performance and car location.</p>
-                                <a className="btn viewbtn rounded-pill shadow" href={face} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'68px'}}>
+                                <h5 className="card-title">ResumeX</h5>
+                                
+                                <p className="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>ResumeX is a website where you can upload your resume and desired job description and the website will recommend changes to match the job desciption or you can use job search to look for a job matching your resume. To try our app click on <a href="https://resumex2020.herokuapp.com "style={{color:'#21AA47'}}>ResumeX.</a> </p>
+                                <a className="btn viewbtn rounded-pill shadow" href="https://github.com/arsala1995/final_project_LHL" target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'68px'}}>
                                     <i className="fa fa-eye">&ensp;View</i>
                                 </a>
                             </div>
@@ -51,10 +53,9 @@ export default class Project extends Component{
                         <div className="card" style={{borderColor:'#f3e909'}} data-aos="fade-up">
                             {/* <img src={facelock} className="card-img" alt="profile picture" style={{height:'100px', weight:'9px', paddingTop:'20px', marginTop:'30px',marginLeft:'25px'}}/> */}
                             <div className="card-body">
-                                <h5 className="card-title">Facial Recognition Smart Lock</h5><br/>
-                                <p className="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>The main purpose of the project is to improve security systems in homes, offices etc. 
-                                The idea is to unlock a door by recognizing the face of an authorized person And then send the image to the cloud for analysis and to notify the user.</p>
-                                <a className="btn viewbtn rounded-pill shadow" href={face} target="_blank" style={{textDecoration:'none', width:'50%'}}>
+                                <h5 className="card-title">SneakerExchange</h5><br/>
+                                <p className="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>This app lets you sell or buy sneakers online. You can use filters to search for sneakers in your location or sizes. You can also upload sneakers by providing all the information regarding your sneakers, the buyer will contact you using email if he is interested and will be marked sold if the sneaker has been sold out.</p>
+                                <a className="btn viewbtn rounded-pill shadow" href="https://github.com/arsala1995/sneaker-exchange" target="_blank" style={{textDecoration:'none', width:'50%'}}>
                                     <i className="fa fa-eye">&ensp;View</i>
                                 </a>
                             </div>
@@ -62,9 +63,9 @@ export default class Project extends Component{
                         <div className="card" style={{borderColor:'#f3e909'}} data-aos="fade-up">
                             {/* <img src="..." className="card-img-top" alt="..."/> */}
                             <div className="card-body">
-                                <h5 className="card-title">Artificial Atmosphere<br/>(ARTMOS)</h5>
-                                <p className="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>To grow crops with suitable surrounding in any environment by creating artificial conditions using micro-controller.</p>
-                                <a className="btn viewbtn rounded-pill shadow" href={artmos} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'102px'}}>
+                                <h5 className="card-title">Jungle</h5>
+                                <p className="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>Jungle is a platform where you can buy anything. Admin has the privilege of uploading and deleting items. So, the buyer can buy online using his/her credit card.</p>
+                                <a className="btn viewbtn rounded-pill shadow" href="https://github.com/arsala1995/Jungle" target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'102px'}}>
                                     <i className="fa fa-eye">&ensp;View</i>
                                 </a>
                             </div>
